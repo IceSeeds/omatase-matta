f7bf4f4a580c988b87dbcefd5c3f37726754abdb
IceSeed <tkm0z@yahoo.co.jp>
2021-01-14 19:58:34 +0900

    class use swiper commit miss

--- js/carousel.js
+++ js/carousel.js
@@ -19,24 +19,23 @@ var mySwiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
       clickable: true,
     },
-    
-    loop: true,
-    autoplay: {
-      delay: 3000, //3秒後に次の画像に代わる
-    }
+    loop: true
 });
-//mySwiper.init;
 
 /*
+TODO
+  別々の動きが出来ない。
+*/
+
 // 複数個でも同じ動きでいいなら、ユニークはイラナイ。
 var mySwiper2 = new Swiper('.swiper-container2', {
-    pagination: {
-      el: '.swiper-pagination2',
-    },
     navigation: {
       nextEl: '.swiper-button-next2',
       prevEl: '.swiper-button-prev2',
     },
+    pagination: {
+      el: '.swiper-pagination2',
+      clickable: true,
+    },
+    loop: true
 });
-mySwiper2.init();
-*/
